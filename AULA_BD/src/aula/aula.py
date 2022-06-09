8a6d56740e580cc8a1e6750e9d5d45264fb879ee
Bruno Lorenzo SALA 901 <bruno.lorenzo.thomaz@gmail.com>
2022-06-08 21:54:32 -0300

    tabela

--- AULA_BD/src/aula/aula.py
+++ AULA_BD/src/aula/aula.py
@@ -1,1 +1,19 @@
+import sqlite3
 
+if __name__ == '__main__':
+
+    conn = sqlite3.connect('clientes.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE clientes (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        nome TEXT NOT NULL,
+        idade INTEGER,
+        cpf VARCHAR(15) NOT NULL,
+        email TEXT NOT NULL,
+        criado_em DATE NOT NULL
+    );
+    """)
+
+    conn.close()
